ce1e7f4607033bb83d5f9996b55f0088e3e33bed
brandoncryu <chryu1@gmail.com>
2020-08-29 15:00:35 -0400

    improved UI

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
+import dash_daq as daq
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
@@ -11,12 +12,9 @@ from sklearn.linear_model import LinearRegression
 from sklearn import model_selection
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SIMPLEX])
 
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-
+##### Initial data handling ########
 housing = pd.read_csv('../Ames_HousePrice.csv', index_col=0)
 #Remove Outliers
 housing = housing[np.logical_and(housing.SalePrice >= 40000, housing.SalePrice <= 750000)]
@@ -27,7 +25,6 @@ housing.SaleType = housing.SaleType.astype('string')
 housing. SaleType = housing.SaleType.str.strip()
 housing = housing[housing.SaleType == 'WD']
 housing = housing[housing.SaleCondition == 'Normal']
-
 
 #Replace NAs
 housing = housing.fillna(0)
@@ -57,7 +54,7 @@ keep = ['LogSalePrice', 'GrLivArea', 'HasGarage', 'HasPool','HasFireplace','Over
 housing = housing[keep]
 
 
-
+##### Fitting linear model with training data #####
 y = housing['LogSalePrice']
 x = housing.drop('LogSalePrice', axis=1)
 
@@ -66,117 +63,135 @@ x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_s
 lm = LinearRegression()
 lm.fit(x_train, y_train)
 
+##### UI components #####
 app.layout = html.Div(
     children=[
-    html.H1(children='Ames Housing Tool'),
+        dbc.Row(
+            dbc.Col(
+                html.H1(children='Ames Housing Tool'),
+                width={"size": 6, "offset": 1}
+            )
+        ),
 
-    html.Div(children='''
-        House hunter toolkit to fit budget.
-    '''),
+        dbc.Row(
+            dbc.Col(
+                html.H5(children='House hunter toolkit to fit their budget.'),
+                width={"size": 6, "offset": 1}
+            )
+        ),
 
-    dbc.Label("Budget"),
-    dcc.Input(
-        id='budget',
-        placeholder='Insert budget',
-        type='number',
-        value=100000,
-        debounce=True,
-        min=12789, max=755000
-    ),
+        html.Br(),
 
-    dbc.Label("Gross Living Area"),
-    dcc.Input(
-        id='GrLivArea',
-        placeholder='Insert living area(ft^2)',
-        type='number',
-        value=1000,
-        debounce=True,
-        min=334, max=4676
-    ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        dbc.Label("Budget"),
+                        daq.NumericInput(
+                            id='budget',
+                            value=100000,
+                            min=12789, max=755000,
+                            size=200
+                        ),
 
-    dbc.Label("Number of cars in garage"),
-    dcc.Input(
-        id='Garage_Cars',
-        placeholder='Number of cars in garage',
-        type='number',
-        value=1,
-        debounce=True,
-        min=0, max=5
-    ),
+                        dbc.Label("Gross Living Area (ft^2)"),
+                        daq.NumericInput(
+                            id='GrLivArea',
+                            value=1000,
+                            min=334, max=4676,
+                            size=200
+                        ),
 
-    dbc.Label("Overall condition"),
-    dcc.Input(
-        id='overall_cond',
-        placeholder='Desired overall condition (1-10)',
-        type='number',
-        value=5,
-        debounce=True,
-        min=0, max=10
-    ),
+                        dbc.Label("Number of cars in garage"),
+                        daq.NumericInput(
+                            id='Garage_Cars',
+                            value=1,
+                            min=0, max=5,
+                            size=200
+                        ),
 
-    dbc.Label("Overall quality"),
-    dcc.Input(
-        id='overall_qual',
-        placeholder='Desired overall quality (1-10)',
-        type='number',
-        value=5,
-        debounce=True,
-        min=0, max=10
-    ),
+                        dbc.Label("Overall condition"),
+                        daq.NumericInput(
+                            id='overall_cond',
+                            value=5,
+                            min=0, max=10,
+                            size=200
+                        ),
+                        
+                        dbc.Label("Overall quality"),
+                        daq.NumericInput(
+                            id='overall_qual',
+                            value=5,
+                            min=0, max=10,
+                            size=200    
+                        ),
+                        html.Br(),
 
-    dcc.Checklist(
-        id='boolean_features',
-        options=[
-            {'label': 'Garage', 'value': 'HasGarage'},
-            {'label': 'Pool', 'value': 'HasPool'},
-            {'label': 'Fire Place', 'value': 'HasFireplace'}
-        ],
-        value=[],
-        labelStyle={'display': 'inline-block'}
-    ),
+                        dbc.Label("Select features you would like in your home"),
+                        dcc.Checklist(
+                            id='boolean_features',
+                            options=[
+                                {'label': 'Garage', 'value': 'HasGarage'},
+                                {'label': 'Pool', 'value': 'HasPool'},
+                                {'label': 'Fire Place', 'value': 'HasFireplace'}
+                            ],
+                            value=[],
+                            labelStyle={'display': 'inline-block'}
+                        ),
 
-    dbc.Label("Neighborhood"),
-    dcc.Dropdown(
-        id='neighborhood',
-        options=[
-            {'label': 'Meadow', 'value': 1},
-            {'label': 'BrDale', 'value': 2},
-            {'label': 'IDOTRR', 'value': 3},
-            {'label': 'BrkSide', 'value': 4},
-            {'label': 'OldTown', 'value': 5},
-            {'label': 'Edwards', 'value': 6},
-            {'label': 'SWISU', 'value': 7},
-            {'label': 'Landmrk', 'value': 8},
-            {'label': 'Sawyer', 'value': 9},
-            {'label': 'NPkVill', 'value': 10},
-            {'label': 'Blueste', 'value': 11},
-            {'label': 'NAmes', 'value': 12},
-            {'label': 'Mitchel', 'value': 13},
-            {'label': 'SawyerW', 'value': 14},
-            {'label': 'Gilbert', 'value': 15},
-            {'label': 'NWAmes', 'value': 16},
-            {'label': 'Greens', 'value': 17},
-            {'label': 'Blmngtn', 'value': 18},
-            {'label': 'CollgCr', 'value': 19},
-            {'label': 'Crawfor', 'value': 20},
-            {'label': 'ClearCr', 'value': 21},
-            {'label': 'Somerst', 'value': 22},
-            {'label': 'Timber', 'value': 23},
-            {'label': 'Veenker', 'value': 24},
-            {'label': 'GrnHill', 'value': 25},
-            {'label': 'StoneBr', 'value': 26},
-            {'label': 'NridgHt', 'value': 27},
-            {'label': 'NoRidge', 'value': 28}
-        ],
+                        dbc.Label("Neighborhood"),
+                        dcc.Dropdown(
+                            id='neighborhood',
+                            options=[
+                                {'label': 'Meadow', 'value': 1},
+                                {'label': 'BrDale', 'value': 2},
+                                {'label': 'IDOTRR', 'value': 3},
+                                {'label': 'BrkSide', 'value': 4},
+                                {'label': 'OldTown', 'value': 5},
+                                {'label': 'Edwards', 'value': 6},
+                                {'label': 'SWISU', 'value': 7},
+                                {'label': 'Landmrk', 'value': 8},
+                                {'label': 'Sawyer', 'value': 9},
+                                {'label': 'NPkVill', 'value': 10},
+                                {'label': 'Blueste', 'value': 11},
+                                {'label': 'NAmes', 'value': 12},
+                                {'label': 'Mitchel', 'value': 13},
+                                {'label': 'SawyerW', 'value': 14},
+                                {'label': 'Gilbert', 'value': 15},
+                                {'label': 'NWAmes', 'value': 16},
+                                {'label': 'Greens', 'value': 17},
+                                {'label': 'Blmngtn', 'value': 18},
+                                {'label': 'CollgCr', 'value': 19},
+                                {'label': 'Crawfor', 'value': 20},
+                                {'label': 'ClearCr', 'value': 21},
+                                {'label': 'Somerst', 'value': 22},
+                                {'label': 'Timber', 'value': 23},
+                                {'label': 'Veenker', 'value': 24},
+                                {'label': 'GrnHill', 'value': 25},
+                                {'label': 'StoneBr', 'value': 26},
+                                {'label': 'NridgHt', 'value': 27},
+                                {'label': 'NoRidge', 'value': 28}
+                            ],
+                            value=12
+                        ),
+                    ],
+                    width={"size": 3, "offset": 1}
+                )
+            ]
+        ),
 
-        value=12
-    ),
+        html.Br(),
+        html.Br(),
 
-
-    html.Div(id='predicted_price')
+        dbc.Row(
+            dbc.Col(
+                html.H4(id='predicted_price'),
+                width={"size": 3, "offset": 1}
+            )
+        )
 ])
 
-
+##### Output functions #####
 @app.callback(
     Output(component_id='predicted_price', component_property='children'),
     [Input(component_id='budget', component_property='value'),
@@ -188,13 +203,9 @@ app.layout = html.Div(
     Input(component_id='neighborhood', component_property='value')
     ]
 )
-def update_output_div(budget_value,GrLivArea_value, Garage_Cars_value, overall_cond_value,overall_qual_value,boolean_features_value,neighborhood_value):
-    if budget_value is None:
-        Budget = 100000
-    else:
-        Budget = budget_value
+def update_predicted_price(budget_value,GrLivArea_value, Garage_Cars_value, overall_cond_value,overall_qual_value,boolean_features_value,neighborhood_value):
+    Budget = budget_value
     LivArea = GrLivArea_value
-    print(LivArea)
     GarageCars =Garage_Cars_value
     OverallQual = overall_qual_value
     OverallCond = overall_cond_value
@@ -216,9 +227,8 @@ def update_output_div(budget_value,GrLivArea_value, Garage_Cars_value, overall_c
     budget = buyer['LogSalePrice']
     buyer_x = buyer.drop('LogSalePrice', axis=1)
     predicted_price = np.exp(lm.predict(buyer_x)[0])
-
     
-    return 'Predicted Price: ${:.2f}'.format(predicted_price)
+    return 'Predicted Price: ${0:,.2f}'.format(predicted_price)
 
 
 if __name__ == '__main__':
